30d489a4103b461f52ef67cdcdee2ceb04b03dca
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-07-31 22:12:18 +0530

    added env file to gitignore

--- server/server.js
+++ server/server.js
@@ -1,31 +1,33 @@
-const express=require('express');
-const mongoose=require('mongoose');
-require('dotenv').config()
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./env.local" });
+const app = express();
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-
-app.get('/',(req,res)=>{
-    res.json({msg:"This is example"});
-})
-app.listen(PORT,()=>{
-    console.log("Server is running...");
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "This is example" });
+});
+app.listen(PORT, () => {
+  console.log("Server is running...");
+});
 
 //routes testing
-app.use('/user',require('./routes/useRouter'))
+app.use("/user", require("./routes/useRouter"));
 
 //connecting mongob
 
-const URI=process.env.MONGODB_URL;
-mongoose.connect(URI,{
-    useCreateIndex:true,
-    useFindAndModify:false,
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-}).then(()=>{
+const URI = process.env.MONGODB_URL;
+mongoose
+  .connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("MongoDB Connected");
-}).catch(err=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
