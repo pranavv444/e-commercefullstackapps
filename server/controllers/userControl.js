8ef7f701347966413172127c24ead11960a1e807
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-08-02 02:35:40 +0530

    added cokkie and access token 6th commit

--- server/controllers/userControl.js
+++ server/controllers/userControl.js
@@ -1,61 +1,80 @@
 const Users = require('../models/userModel');
-const catchAsyncErrors = require("../utils/catchAysncErrors")
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 const userControl = {
     register: async (req, res) => {
         try {
             const { name, email, password } = req.body;
-       
 
+            // Check if user already exists
             const user = await Users.findOne({ email });
             if (user) {
                 return res.status(400).json({ msg: "Email Already Registered" });
             }
+            // Validate password length
             if (password.length < 6) {
                 return res.status(400).json({ msg: "Password must be at least 6 characters" });
             }
 
+            // Password encryption
+            const passwordHash = await bcrypt.hash(password, 10);
+
+            // Create new user
             const newUser = new Users({
                 name,
                 email,
-                password
+                password: passwordHash
             });
 
+            // Save user to database
             await newUser.save();
-            res.json({ msg: "Registration Success" });
+
+            // Create JWT tokens
+            const accesstoken = createAccessToken({ id: newUser._id });
+            const refreshtoken = createRefreshToken({ id: newUser._id });
+
+            // Set refresh token as cookie
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+            });
+
+            // Send access token and success message
+            res.json({ accesstoken, msg: "Registration Success" });
             console.log(newUser);
 
         } catch (err) {
+            console.error(err); // Log error to console
             return res.status(500).json({ msg: err.message });
         }
-    }
+    },
+    // refreshtoken: async (req, res) => {
+    //     try {
+    //         const rf_token = req.cookies.refreshtoken;
+    //         if (!rf_token) return res.status(400).json({ msg: "Please Login or Register" });
+
+    //         jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    //             if (err) return res.status(400).json({ msg: "Please Login or Register" });
+
+    //             const accesstoken = createAccessToken({ id: user.id });
+    //             res.json({ accesstoken });
+    //         });
+    //     } catch (err) {
+    //         console.error(err); // Log error to console
+    //         return res.status(500).json({ msg: err.message });
+    //     }
+    // }
 }
 
+const createAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+}
 
-// const userControl= {
-//     register: catchAsyncErrors(async (req, res,next) => {
-     
-//             const { name, email, password } = req.body;
-       
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+}
 
-//             const user = await Users.findOne({ email });
-//             if (user) {
-//                 return res.status(400).json({ msg: "Email Already Registered" });
-//             }
-//             if (password.length < 6) {
-//                 return res.status(400).json({ msg: "Password must be at least 6 characters" });
-//             }
-
-//             const newUser = new Users({
-//                 name,
-//                 email,
-//                 password
-//             });
-
-//             await newUser.save();
-//             res.json({ msg: "Registration Success" });
-//             console.log(newUser);
-
-//        next();
-//     })
-// }
 module.exports = userControl;
+
