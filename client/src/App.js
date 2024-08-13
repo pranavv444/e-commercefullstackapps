72a1f0909a8bf879fd1c55af86eabbdab369e6a6
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-08-14 03:41:42 +0530

    made navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Header from './components/headers/Header'
 import Pages from './components/mainpages/Pages'
+import {BrowserRouter as  Router } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-
+    <Router>
+    <div className='App'>
     <Header/>
     <Pages/> 
 
     </div>
+    </Router>
   )
 }
 
--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -2,10 +2,32 @@ import React from 'react'
 import { CiMenuBurger } from "react-icons/ci";
 import { IoMdClose } from "react-icons/io";
 import { FaCartPlus } from "react-icons/fa6";
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header></header>
+    <header>
+        <div className='menu'>
+            <CiMenuBurger size={30}/>
+        </div>
+        <div className='logo'>
+            <h1>
+                <Link to='/'>30 Shop</Link>
+            </h1>
+        </div>
+        <ul>
+            <li><Link to='/'>Products</Link></li>
+            <li><Link to='/login'>Login or Register</Link></li>
+            <li>
+                <IoMdClose size={30} className='menu'/>
+            </li>
+
+        </ul>
+        <div className='cart-icon'>
+            <span>0</span>
+            <Link><FaCartPlus size={25}/></Link>
+        </div>
+    </header>
   )
 }
 
