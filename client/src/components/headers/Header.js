8a3935e94605aff5b1c3aef7c438d578a396cafa
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-08-13 23:19:54 +0530

    started working on frontend

---
+++ client/src/components/headers/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CiMenuBurger } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
+import { FaCartPlus } from "react-icons/fa6";
+
+const Header = () => {
+  return (
+    <header></header>
+  )
+}
+
+export default Header
