85a722b67e8a4b713dca8000a845d393c4f47876
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-08-20 03:19:19 +0530

    test/product.js-2

--- client/src/api/helpers/useGetProducts.js
+++ client/src/api/helpers/useGetProducts.js
@@ -1,13 +1,15 @@
 import {useQuery} from '@tanstack/react-query'
 import axios from 'axios'
-
+// ,
+//   "proxy": "https://e-commercefullstackapps.onrender.com/"
+const base='https://e-commercefullstackapps.onrender.com'
 const url='/api/products';
 export const useGetProducts = () => {
     const getAllProducts=async()=>{
         try {
-            const res=await axios.get(url)//chalra hai  
+            const res=await axios.get(base+url)//chalra hai  
               console.log("product api calls:", res.data.result)
-              
+              console.log("base+url",base+url)
 
         return res.data.result;
         } catch (error) {
