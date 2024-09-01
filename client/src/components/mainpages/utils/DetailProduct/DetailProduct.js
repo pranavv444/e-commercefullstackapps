01a28de829e22a659e56d68972175e876ec92a0b
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-09-01 23:59:25 +0530

    fix/detail product

--- client/src/components/mainpages/utils/DetailProduct/DetailProduct.js
+++ client/src/components/mainpages/utils/DetailProduct/DetailProduct.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import { GlobalState } from '../../../../GlobalState';
 
 const DetailProduct = () => {
   const location = useLocation();
   const { title, _id, images, price, description, content, sold } = location.state.product;
+  const state = useContext(GlobalState);
+  const addCart = state.userAPI.addCart;
+
+  const handleAddToCart = () => {
+    addCart(location.state.product);
+  };
 
   return (
     <div className='detail'>
@@ -18,7 +24,7 @@ const DetailProduct = () => {
         <p>{description}</p>
         <p>{content}</p>
         <p>Sold: {sold}</p>
-        <Link to='/cart' className='cart'>BUY NOW</Link>
+        <Link to='/cart' className='cart' onClick={handleAddToCart}>BUY NOW</Link>
       </div>
     </div>
   );
